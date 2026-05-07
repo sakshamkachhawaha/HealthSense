a2a79ea6712f63c66f1c6716d6aceb7f1569a895
sakshamkachhawaha <sakshamkachhawaha2007@gmail.com>
2026-05-07 18:41:03 +0530

    Report UI Done

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,40 +1,29 @@
 import React from 'react'
+import { FolderOpen } from 'lucide-react';
+
 
 const Reports = () => {
   return (
     <>
-    <div className='bg-gray-500 w-full h-full p-6'>
-      <div className=' bg-red-500 flex flex-row space-evenly p-4 gap-160'>
+    <div className='bg-white w-full h-full p-6'>
+      <div className=' bg-white flex flex-row space-evenly p-4 gap-160'>
         <div>
-          <h1 className='text-white text-4xl font-serif font-medium'>Health Reports</h1>
-          <p className='pt-1 text-white text-medium font-serif font-medium'>Your complete history of health assessments</p>
+          <h1 className='text-black text-4xl font-serif font-thin'>Health Reports</h1>
+          <p className='pt-1 text-gray-600 text-medium font-serif font-thin'>Your complete history of health assessments</p>
         </div>
-        <div className='align-right bg-[#43664D] w-41 h-13 border-2 border-[#43664D] rounded-3xl flex items-center justify-center'>
-          <button className="text-white text-medium" onClick={() => navigate('/check')}>
-          <span className="text-white text-medium">+ </span>
-          New Check
-        </button>
+        <div className='align-right bg-emerald-600 w-41 h-13 border-2 border-[#43664D] rounded-3xl flex items-center justify-center'>
+          <button className="text-white text-medium transform transition duration-300 hover:cursor-pointer hover:scale-95" onClick={() => navigate('/check')}>
+          <span className="text-white text-medium mr-2">+ </span>
+           New Check
+          </button>
         </div>
       </div>
-      <div className='flex flex-column space-evenly p-4 gap-4 items-center justify-center h-full w-full'>
-        <div className='flex items-center'>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            style={{ fill: '#FFFFFF' }}
-          >
-            <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Z"/>
-          </svg>
-        </div>
-        <div>
-          <h3>No reports yet</h3>
-        </div>
-        <div>
-          <p>Complete your first health check to see your reports here</p>
-        </div>
-    </div>
+      <div className='flex items-center w-full flex-col justify-center h-90 bg-[#F3F4F0] mt-4 rounded-3xl text-white text-2xl font-serif font-thin'>
+        <FolderOpen size={60} className='text-gray-600'/>
+        <p className='font-serif font-thin mt-4 text-gray-800'>No reports yet</p>
+        <p className='font-sans font-thin mt-4 text-lg text-gray-900'>Complete your first health check to see your reports here.</p>
+        <button className='bg-emerald-600 px-9 py-3 mt-4 rounded-3xl text-xl font-sans transform transition duration-300 hover:cursor-pointer hover:scale-95 '>Start Health Check</button>
+      </div>
     </div>
     </>
   )
